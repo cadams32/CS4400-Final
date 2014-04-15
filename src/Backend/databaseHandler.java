d59285a800457f24d3b3c9f450debb036349c578
Jonathan Elliott <jonathanpierceelliott@gmail.com>
2014-04-15 18:32:46 -0400

    DBHandler Login moar stuff

--- src/Backend/databaseHandler.java
+++ src/Backend/databaseHandler.java
@@ -1,22 +1,52 @@
 package Backend;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class databaseHandler {
 	
-	private Connection connection;
+	private static Connection connection;
 	
 	public databaseHandler() {
 		dataBaseConnection DBC = new dataBaseConnection();
 		connection = DBC.createConnection();
 	}
-	
-	public static void login(String username, String password) {
+
+	private static boolean login(String username, String password) throws SQLException {
+		boolean result = false;
+		Statement stmt = null;
+		String query = "SELECT User FROM User WHERE User.Username=username AND User.Password=password";
 		try {
-		
-		} catch(Exception e) {
-			
+			stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			String dbU = "";
+			String dbP = "";
+			while(rs.next()) {
+				dbU = rs.getString("Username");
+				dbP = rs.getString("Password");
+			}
+			if(dbU != null && dbP != null && username.equals(dbU) && password.equals(dbP)) {
+				result = true;
+			}
+		} catch (SQLException e) {
+				e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
 		}
+		return result;
+	}
+	
+	private static boolean searchDoctorForUser(String username) {
+		return false;
+	}
+	
+	private static boolean searchPatientForUser(String username) {
+		return false;
 	}
 	
 
