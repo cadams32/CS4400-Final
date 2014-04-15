11d274da889a2c867ea2e041a821f3d8c0867d97
Jonathan Elliott <jonathanpierceelliott@gmail.com>
2014-04-15 16:57:59 -0400

    DBConnection. I believe this should work. Need to test it out.

--- src/Backend/dataBaseConnection.java
+++ src/Backend/dataBaseConnection.java
@@ -3,22 +3,31 @@ import java.sql.*;
 
 
 public class dataBaseConnection {
-	private static final String connection = "";
-	private static final String driverName = "";
+	private static final String connection = "jdbc:mysql://academic-mysql.cc.gatech.edu/phpmyadmin/cs4400_Group_37";
+	private static final String driverName = "com.mysql.jdbc.Driver";
 	private static final String username = "cs4400_Group_37";
-	private static final String password = "";
-
-	public dataBaseConnection() {
-	}
+	private static final String password = "Vs2GEguW";
 	
 	public Connection createConnection() {
+		
 		Connection conn = null;
+		
 		try {
-			if (!conn.isClosed()) {
-				return conn;
-			}
+			Class.forName(driverName);
+		
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			conn = DriverManager.getConnection(connection, username, password);
 		} catch (SQLException e) {
-			
+			e.printStackTrace();
+		}
+		
+		if(conn != null) {
+			return conn;
+			//Success!!!
 		}
 		
 		return null;
@@ -30,7 +39,7 @@ public class dataBaseConnection {
 				conn.close();
 			}
 		} catch (SQLException e) {
-			
+			e.printStackTrace();
 		}
 	}
 	
