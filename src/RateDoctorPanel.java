39f9c3a2393e2d48ee2d91ff24be7b02cfe79ba7
cadams32 <cadams32@gatech.edu>
2014-04-17 20:58:12 -0400

    Revert "Made a few modifications to DocRater"
    This reverts commit 2b32671beede8cf54e1112155f9f0e07589d9f93.

--- src/RateDoctorPanel.java
+++ src/RateDoctorPanel.java
@@ -7,9 +7,6 @@ import javax.swing.JComboBox;
 import javax.swing.JSlider;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.SystemColor;
-import java.awt.Font;
 
 
 public class RateDoctorPanel extends JPanel {
@@ -23,36 +20,34 @@ public class RateDoctorPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public RateDoctorPanel(MedicalFrame parent, String username) {
+		
 		this.parent = parent;
 		this.username = username;
 		
-		setBackground(SystemColor.textHighlight);
-		setLayout(null);
+		setLayout(new MigLayout("", "[][][][][][][][][][][][][][grow]", "[][][][][][][][][]"));
 		
-		JLabel lblRateADoctor = new JLabel("Rate A Doctor!");
-		lblRateADoctor.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
-		lblRateADoctor.setBounds(385, 20, 289, 52);
-		add(lblRateADoctor);
+		JLabel lblRateADoctor = new JLabel("Rate a Doctor");
+		add(lblRateADoctor, "cell 13 1");
+		
+		JLabel lblDoctor = new JLabel("Doctor");
+		add(lblDoctor, "cell 12 6,alignx trailing");
+		
+		JComboBox comboBox = new JComboBox();
+		add(comboBox, "cell 13 6");
+		
+		JLabel lblRating = new JLabel("Rating");
+		add(lblRating, "cell 12 7");
 		
 		slider = new JSlider();
-		slider.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		slider.setSnapToTicks(true);
 		slider.setPaintTicks(true);
 		slider.setPaintLabels(true);
 		slider.setMaximum(5);
 		slider.setMajorTickSpacing(1);
-		slider.setBounds(243, 216, 549, 52);
-		add(slider);
-		
-		JLabel lblRating = new JLabel("Rating: ");
-		lblRating.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
-		lblRating.setBounds(105, 201, 126, 47);
-		add(lblRating);
+		add(slider, "cell 13 7");
 		
 		JButton btnRate = new JButton("Rate");
-		btnRate.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		btnRate.setBounds(429, 334, 117, 29);
-		add(btnRate);
+		add(btnRate, "cell 13 8");
 
 	}
 	
@@ -69,4 +64,6 @@ public class RateDoctorPanel extends JPanel {
 		
 		
 	}
+	
+
 }
