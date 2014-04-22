bf38fadcf6925a96b83ce33a0b0af9da8e99290d
Jonathan Elliott <jonathanpierceelliott@gmail.com>
2014-04-22 01:23:26 -0400

    More Updates/Changes to DBHandler/Conn

--- src/Backend/DataBaseConnection.java
+++ src/Backend/DataBaseConnection.java
@@ -12,14 +12,12 @@ public class DataBaseConnection {
             System.out.println("Driver Success");
             conn = DriverManager.getConnection("jdbc:mysql://130.207.114.235/cs4400_Group_37", "cs4400_Group_37", "Vs2GEguW"); 
             if(!conn.isClosed()) {
-                System.out.println("Successfully connected to " + 
-                "MySQL server using TCP/IP..."); 
+                System.out.println("Successfully connected to MySQL server using TCP/IP..."); 
                 return conn;
             }
         } catch(Exception e) { 
             System.err.println("Exception: " + e.getMessage()); 
         }
-        System.out.println("Not Swag");
         return null;
     }
 	
--- src/Backend/DatabaseHandler.java
+++ src/Backend/DatabaseHandler.java
@@ -8,11 +8,15 @@ import java.sql.Statement;
 /**
  * Creates queries for manipulating the DB:
  * 
+ * LOGIN
  * 		- login - String - the type of user being logged in "Patient", "Doctor", "Invalid"
  *			- validateLogin - boolean - searches the User table for username and password combination
  *			- searchDoctorForUser - boolean - searches the Doctor table for specific user
  *			- searchPatientForUser - boolean - searches the Patient table for specific user
- *		- doesUsernameExist - boolean - returns true if the username exists in the User table
+ *		
+ *
+ * NEW USER
+ * - doesUsernameExist - boolean - returns true if the username exists in the User table
  *		- addNewUser - void - inserts new User into User table with given params
  *		- addNewPatient - void - inserts new Patient into Patient table with given params
  *		- addNewDoctor - void - inserts new Doctor into Doctor table with given params
@@ -28,43 +32,42 @@ public class DatabaseHandler {
 		DataBaseConnection DBC = new DataBaseConnection();
 	}
 	
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ----------------------------LOGIN------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------------------
+	
 	/**
-	 * Wanted to return the type of user as a string.
+	 * Login returns "Patient", "Doctor", "Admin", or "Invalid" based on the results
+	 * @param username
+	 * @param password
+	 * @return
 	 */
 	public static String login(String username, String password) {
-		connection = DBC.createConnection();
+	
 		if(validateLogin(username, password)) {
-			DBC.closeConnection(connection);
-			if(searchPatientForUser(username)) {
-				DBC.closeConnection(connection);
+			if(searchPatientForUser(username)) {		
 				return "Patient";
 			}
 			if(searchDoctorForUser(username)) {
-				DBC.closeConnection(connection);
 				return "Doctor";
 			}
-			DBC.closeConnection(connection);
 			return "Admin";
 		}
-		DBC.closeConnection(connection);
 		return "Invalid";
 	}
 	
 	/**
-	 * 
-	 * 
+	 * Check if Username and Password both match in User table
 	 * @param username
 	 * @param password
 	 * @return
 	 */
 	private static boolean validateLogin(String username, String password) {
+	
 		boolean result = false;
-		
-		System.out.println(username + " " + password);
-		
 		String query = "SELECT * FROM `User` WHERE `Username` = '" + username + "' AND `Password` =  '" + password + "'";
-		
 		try {
+			connection = DBC.createConnection();
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = (ResultSet) statement.executeQuery(query);
 			String dbU = "";
@@ -73,7 +76,11 @@ public class DatabaseHandler {
 				dbU = resultSet.getString("Username");
 				dbP = resultSet.getString("Password");
 			}
+			resultSet.close();
+			statement.close();
+			DBC.closeConnection(connection);
 			if(dbU != null && dbP != null && username.equals(dbU) && password.equals(dbP)) {
+				
 				result = true;
 			}
 		} catch (Exception e) {
@@ -84,20 +91,22 @@ public class DatabaseHandler {
 	
 	/**
 	 * Search for a specific Doctor based on user
-	 * 
 	 * @param username
 	 * @return boolean if found
 	 */
 	private static boolean searchDoctorForUser(String username) {
 		String query = "SELECT `DocUsername` FROM `Doctor` WHERE `DocUsername` = '" + username + "'";
 		try {
+			connection = DBC.createConnection();
 			Statement statement = connection.prepareStatement(query);
 			ResultSet resultSet = (ResultSet) statement.executeQuery(query);
 			String dbU = "";
 			while(resultSet.next()) {
 				dbU = resultSet.getString("DocUsername");
 			}
+			resultSet.close();
 			statement.close();
+			DBC.closeConnection(connection);
 			if(dbU != null && username.equals(dbU)) {
 				return true;
 			}
@@ -108,21 +117,23 @@ public class DatabaseHandler {
 	}
 	
 	/**
-	 * search Patient for User
-	 * 
+	 * Search Patient for User
 	 * @param username
 	 * @return
 	 */
 	private static boolean searchPatientForUser(String username) {
 		String query = "SELECT `PatientUsername` FROM `Patient` WHERE `PatientUsername` = '" + username + "'";
 		try {
+			connection = DBC.createConnection();
 			Statement statement = connection.prepareStatement(query);
 			ResultSet resultSet = (ResultSet) statement.executeQuery(query);
 			String dbU = "";
 			while(resultSet.next()) {
 				dbU = resultSet.getString("PatientUsername");
 			}
+			resultSet.close();
 			statement.close();
+			DBC.closeConnection(connection);
 			if(dbU != null && username.equals(dbU)) {
 				return true;
 			}
@@ -132,24 +143,28 @@ public class DatabaseHandler {
 		return false;
 	}
 	
-	//---------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------------------
+	// ----------------------------INSERTING NEW USERS INTO THE DATABASE----------------------------------------------------
+	// ---------------------------------------------------------------------------------------------------------------------
 	
 	/**
-	 * Check if username exists.
-	 * 
+	 * Check if Username exists.
 	 * @param username
 	 * @return
 	 */
 	private static boolean doesUsernameExist(String username) {
 		String query = "SELECT  `Username` FROM  `User` WHERE  `Username` = '" + username + "'";
 		try {
+			connection = DBC.createConnection();
 			Statement statement = connection.prepareStatement(query);
 			ResultSet resultSet = (ResultSet) statement.executeQuery(query);
 			String dbU = "";
 			while(resultSet.next()) {
 				dbU = resultSet.getString("Username");
 			}
+			resultSet.close();
 			statement.close();
+			DBC.closeConnection(connection);
 			if(dbU != null && username.equals(dbU)) {
 				return true;
 			}
@@ -161,22 +176,67 @@ public class DatabaseHandler {
 	}
 	
 	/**
-	 * Insert into User
-	 * 
+	 * Insert New User
 	 * @param username
 	 * @param password
 	 */
-	public static void addUser(String username, String password) {
-		connection = DBC.createConnection();
+	public static void addNewUser(String username, String password) {
 		if(!doesUsernameExist(username)) {
 			String query = "INSERT INTO  `cs4400_Group_37`.`User` (`Username` ,`Password`) VALUES ('"+username+"',  '"+password+"')";
 			try {
+				connection = DBC.createConnection();
 				Statement statement = connection.createStatement();
 				statement.executeUpdate(query);
+				statement.close();
+				DBC.closeConnection(connection);
 			} catch (Exception e) {
 				System.err.println("Exception: " + e.getMessage());
 			}
 		}
 		DBC.closeConnection(connection);
 	}
+	
+	/**
+	 * Insert New Patient
+	 * @param username
+	 * @param password
+	 */
+	public static void addNewPatient(String username, String password) {
+		if(!doesUsernameExist(username)) {
+			String query = "";
+			try {
+				connection = DBC.createConnection();
+				Statement statement = connection.createStatement();
+				statement.executeUpdate(query);
+				statement.close();
+				DBC.closeConnection(connection);
+			} catch (Exception e) {
+				System.err.println("Exception: " + e.getMessage());
+			}
+		}
+	}
+	
+	/**
+	 * Insert New Doctor
+	 * @param username
+	 * @param password
+	 */
+	public static void addNewDoctor(String username, String password) {
+		if(!doesUsernameExist(username)) {
+			String query = "";
+			try {
+				connection = DBC.createConnection();
+				Statement statement = connection.createStatement();
+				statement.executeUpdate(query);
+				statement.close();
+				DBC.closeConnection(connection);
+			} catch (Exception e) {
+				System.err.println("Exception: " + e.getMessage());
+			}
+		}
+	}
+	
+	
+	
+	
 }
